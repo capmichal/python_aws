fca9c31b9881fbd6f850fe7a887ccc50ca032921
Michal Olejnik <michal.olejnik@capgemini.com>
2023-07-21 09:29:21 +0200

    basic webapp setup running on port 5000

--- webapp.py
+++ webapp.py
@@ -1,1 +1,11 @@
 from flask import Flask
+
+
+app = Flask(__name__) # creating Flask object
+
+@app.route('/') # decorator that turns python function into a Flask view function 
+def index():
+    return "Hello world"
+
+
+app.run(host="0.0.0.0", port=5000)
